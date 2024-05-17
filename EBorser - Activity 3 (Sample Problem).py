ed151778f7acc4f10b8676e0eeaff3071b93ccac
Erica Borser <erica.borser.09@gmail.com>
2024-05-17 10:23:43 +0800

    Add files via upload

---
+++ EBorser - Activity 3 (Sample Problem).py
@@ -0,0 +1,27 @@
+"""
+Created on Sat Feb 17 16:56:44 2024
+
+@author: Erica Borser
+"""
+
+#Calculate the probability of cancer patient and diagnostic test
+#Calculate P(A|B) given P(A), P(B|A), P(B|not A)
+def bayes_theorem(p_a, p_b_given_a, p_b_given_not_a):
+    #calculate P(not A)
+    not_a = 1 - p_a
+    #calculate P(B)
+    p_b = p_b_given_a * p_a + p_b_given_not_a * not_a
+    #calculate P(A|B)
+    p_a_given_b = (p_b_given_a * p_a) / p_b
+    return p_a_given_b
+
+#P(A)
+p_a = 0.0002
+#P(B|A)
+p_b_given_a = 0.85
+#P(B|not A)
+p_b_given_not_a = 0.05
+#Calculate P(A|B)
+result = bayes_theorem(p_a, p_b_given_a, p_b_given_not_a)
+#Summarize
+print('P(A|B) = %.3f%%' % (result *100))
